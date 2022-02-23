941c327d9eb1e6e0148e4aec802f9bd69b692175
Goran Jurić <74900926+gogi640@users.noreply.github.com>
2022-02-23 12:13:18 +0100

    Update getDistance_easyC.ino

--- examples/getDistance_easyC/getDistance_easyC.ino
+++ examples/getDistance_easyC/getDistance_easyC.ino
@@ -14,8 +14,8 @@ Ultrasonic_Sensor hc; //Initializer for sensor with easyC
 
 void setup()
 {
-  Serial.begin(115200);
-  hc.begin();
+  Serial.begin(115200);  //Start serial communication with PC via UART
+  hc.begin();  //Initialize sensor
 }
 
 void loop()
